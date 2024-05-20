52b91f08c5949367a311fdc0d289f64cb1c6a5f9
Bammez <Kingnuel4@gmail.com>
2024-05-20 13:19:49 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut multiples = std::collections::HashSet::new();
+
+    for &factor in factors {
+        if factor != 0 {
+            for i in 1..(limit / factor) {
+                multiples.insert(factor * i);
+            }
+        }
+    }
+
+    multiples.iter().sum()
 }
+
+fn main() {
+    let limit = 20;
+    let factors = vec![3, 5];
+    let result = sum_of_multiples(limit, &factors);
+    println!("Sum of multiples: {}", result); // Output: 78
+}
+
+
+
